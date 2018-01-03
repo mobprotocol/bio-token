79f3fc4554646dcf2c9071a04e376b2b76c0f737
Sean Pollock <seanpollock3344@gmail.com>
2018-01-03 13:15:50 -0600

    extracting schema from data

--- utils/data_to_json.py
+++ utils/data_to_json.py
@@ -1,13 +1,23 @@
 import os
+import json
 
 def create_json_schema():
+    """ returns json schema for aligned images data"""
     data_schema = {}
     data_path = './data/youtube_faces/aligned_images_DB/'
     for person in os.listdir(data_path):
-        if person.startswith('.'):
-            continue
-        for imageset in os.listdir(data_path + person):
-            for image in os.listdir(data_path + person + '/' + imageset):
-                print(image)
+        if not person.startswith('.'):
+            data_schema[person] = {}
+            for imageset in os.listdir(data_path + person):
+                if not imageset.startswith('.'):
+                    data_schema[person][imageset] = []
+                    for image in os.listdir(data_path + person + '/' + imageset):
+                        data_schema[person][imageset].append(image)
+    print(data_schema)
 
-create_json_schema()
+def write_schema(schema):
+    """ writes schema to json file """
+
+
+schema = create_json_schema()
+write_schema
