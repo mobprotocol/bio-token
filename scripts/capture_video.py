36f86edbe6a91c4d54964b52af3183f52423254f
Sean Pollock <seanpollock3344@gmail.com>
2018-01-08 10:58:39 -0600

    fixing video write bug

--- scripts/capture_video.py
+++ scripts/capture_video.py
@@ -10,6 +10,13 @@ if not os.path.exists('./data/captured'):
     except OSError as e:
         raise
 
+# make sure ./data/captured/raw_video dir exists
+if not os.path.exists('./data/captured/raw_video'):
+    try:
+        os.makedirs('./data/captured/raw_video')
+    except OSError as e:
+        raise
+
 # get name
 first_name = input('enter your first name: ')
 last_name = input('enter your last name: ')
@@ -18,20 +25,19 @@ capture_duration = 10 # seconds
 capture = cv2.VideoCapture(0)
 
 # codec an videowriter
-fourcc = cv2.VideoWriter_fourcc(*'XVID')
-out = cv2.VideoWriter('data/captured/' + first_name + '_' + last_name + '.avi',fourcc, 20.0, (640,480))
-
+fourcc = cv2.VideoWriter_fourcc(*'MJPG')
+out = cv2.VideoWriter('data/captured/raw_video/' + first_name + '_' + last_name + '.avi', fourcc, 20.0, (1280, 720))
+video_counter = 0
 # capture video for 10 seconds
 start_time = time.time()
 while(time.time() - start_time < capture_duration):
     # catpure video frames
     ret, frame = capture.read()
+    h, w, channels = frame.shape
     if ret==True:
-        frame = cv2.flip(frame, 0)
-        out.write(frame)
-
         # manipulate frames
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        out.write(frame)
 
         # display
         cv2.imshow('frame', gray)
@@ -39,6 +45,6 @@ while(time.time() - start_time < capture_duration):
     else:
         break
 
-capture.release()
 out.release()
+capture.release()
 cv2.destroyAllWindows()
