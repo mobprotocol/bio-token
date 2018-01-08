bf2651c2d10f4dd24778519003b96aaf1cda1a67
Sean Pollock <seanpollock3344@gmail.com>
2018-01-08 13:42:50 -0600

    face and eye detection via haar cascades working

--- scripts/find_faces.py
+++ scripts/find_faces.py
@@ -19,12 +19,12 @@ def find_faces(frame):
         cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
         roi_gray = gray[y:y+h, x:x+w]
         roi_color = frame[y:y+h, x:x+w]
-    #     eyes = eye_cascade.detectMultiScale(roi_gray)
-    #     for (ex,ey,ew,eh) in eyes:
-    #         cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+        eyes = eye_cascade.detectMultiScale(roi_gray)
+        for (ex,ey,ew,eh) in eyes:
+            cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
 
     cv2.imshow('frame', frame)
-    cv2.waitKey(100)
+    cv2.waitKey(1)
     cv2.destroyAllWindows()
 
 capture = cv2.VideoCapture('./data/captured/raw_video/sean_pollock.avi')
