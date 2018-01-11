a4304d32b867f81d431874c594b3cb3d986eff95
Sean Pollock <seanpollock3344@gmail.com>
2018-01-11 13:41:24 -0600

    error handling and cleanup

---
+++ scripts/analyze_video.py
@@ -0,0 +1,44 @@
+import sys
+import cv2
+from find_faces import find_faces
+
+def main():
+    '''
+        allows you to view face and eye detection of .avi video
+        python3 ./scripts/analyze_video.py sean pollock
+        args:
+            first_name
+            last_name
+    '''
+
+    # fetch video from file s ystem
+    capture = cv2.VideoCapture('./data/captured/raw_video/{0}_{1}.avi'.format(sys.argv[1], sys.argv[2]))
+
+    # iterate through video
+    while(True):
+
+        # get frame from video
+        ret, frame = capture.read()
+
+        if ret == True:
+            # find face usint util fn
+            face_image = find_faces(frame)
+
+            # display detectd features
+            cv2.imshow('face_image', face_image)
+            cv2.waitKey(1)
+
+        # break when video is done
+        else:
+            break
+
+    # release video
+    capture.release()
+    cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+    # make sure we have first and last name
+    assert len(sys.argv) == 3
+
+    # start script
+    main()
