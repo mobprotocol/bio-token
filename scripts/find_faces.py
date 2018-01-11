821069fe298901c41640e6b092a98dc7080fb783
Sean Pollock <seanpollock3344@gmail.com>
2018-01-11 12:02:13 -0600

    removing watch video method

--- scripts/find_faces.py
+++ scripts/find_faces.py
@@ -1,31 +1,46 @@
 import numpy as np
 import cv2
 
-def watch_video(vido):
+def find_faces(frame):
+    # pre-trained opencv haar cascade algorithm \
+    face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+    eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
+
+    # turn frame to gray scale
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # detect face/s in picture with haar cascade
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+
+    #draw rectangle around face
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
+
+        # detect eyes
+        eyes = eye_cascade.detectMultiScale(roi_gray)]
+
+        # draw rectangle around eyes
+        for (ex,ey,ew,eh) in eyes:
+            cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+
+    # display detected features
+    cv2.imshow('frame', frame)
+    cv2.waitKey(1)
+
+    # end session
+    cv2.destroyAllWindows()
+
+def main():
+
+if __name__ = '__main__':
+    # given first and list name
+    assert len(sys.argv) == 3
+
+    # fetch video from file sytsem
+    capture = cv2.VideoCapture('./data/captured/raw_video/{0}_{1}.avi'.format(sys.argv[0], sys.argv[1]))
+
+    # iterate through video
     while(1):
             ret, frame = capture.read()
             if ret == True:
                 find_faces(frame)
-
-
-def find_faces(frame):
-    face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-    eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
-
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-    for (x, y, w, h) in faces:
-        cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
-        roi_gray = gray[y:y+h, x:x+w]
-        roi_color = frame[y:y+h, x:x+w]
-        eyes = eye_cascade.detectMultiScale(roi_gray)
-        for (ex,ey,ew,eh) in eyes:
-            cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
-
-    cv2.imshow('frame', frame)
-    cv2.waitKey(1)
-    cv2.destroyAllWindows()
-
-capture = cv2.VideoCapture('./data/captured/raw_video/sean_pollock.avi')
-watch_video(capture)
