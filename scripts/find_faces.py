c56698f975740f4ed7d08b35ee0d10b65acc59f4
Sean Pollock <seanpollock3344@gmail.com>
2018-01-11 13:52:27 -0600

    fixing eye detection framing

--- scripts/find_faces.py
+++ scripts/find_faces.py
@@ -9,29 +9,29 @@ def find_faces(frame):
         args:
             frame: cv2 image instance
     '''
-    
+
     # pre-trained opencv haar cascade algorithm \
     face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
     eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
 
     # turn frame to gray scale
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray_image = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     # detect face/s in picture with haar cascade
-    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    faces = face_cascade.detectMultiScale(gray_image, 1.3, 5)
 
     #draw rectangle around face
     for (x, y, w, h) in faces:
-        cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
+        cv2.rectangle(gray_image,(x,y),(x+w,y+h),(255,0,0),2)
+
+        # crop close-up
+        close_up = gray_image[y:y+h, x:x+w]
 
         # detect eyes
-        eyes = eye_cascade.detectMultiScale(roi_gray)]
+        eyes = eye_cascade.detectMultiScale(close_up)
 
         # draw rectangle around eyes
-        for (ex,ey,ew,eh) in eyes:
-            cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+        for (ex, ey, ew, eh) in eyes:
+            cv2.rectangle(close_up, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 2)
 
-            if ret == True:
-                find_faces(frame)
-
-    return frame
+    return gray_image
