d3cd758636b63951729ad0b128902c9fc0268aa3
Sean Pollock <seanpollock3344@gmail.com>
2018-01-12 17:33:24 -0600

    feature extraction method

--- scripts/feature_detection.py
+++ scripts/feature_detection.py
@@ -2,6 +2,41 @@ import cv2
 import dlib
 import numpy
 import numpy as np
+import json
+
+def detect_features(image):
+    '''
+        given an image finds landmark features
+        args:
+            cv2 image
+        returns:
+            flat list of landmark feature points
+                63 * 2
+    '''
+
+    # instnatiate dlib face detector
+    detector = dlib.get_frontal_face_detector()
+
+    # instantiate dlib facial feature finder
+    predictor = dlib.shape_predictor('./dlib_shape_predictor.dat') # .dat is a pretrained model
+
+    # find faces with pretrained dlib detector
+    faces = detector(image, 1)
+
+    # all iamges should have only 1 face
+    assert len(faces) == 1
+
+    # iterate throuch detected faces
+    for (i, face) in enumerate(faces):
+
+        # get landmarks from image
+        raw_shape = predictor(image, face)
+
+        # process landmark coordinates
+        shape = shape_to_np(raw_shape)
+
+        return shape.tolist()
+
 
 def convert_rect(frame):
     '''
