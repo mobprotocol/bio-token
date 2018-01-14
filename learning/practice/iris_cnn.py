8a4a71a2b22b4a16758eb531c1b3891955b5eb6f
Sean Pollock <seanpollock3344@gmail.com>
2018-01-13 20:27:32 -0600

    organizing practice work

---
+++ learning/practice/iris_cnn.py
@@ -0,0 +1,80 @@
+import os
+from six.moves.urllib.request import urlopen
+import csv
+import numpy as np
+import tensorflow as tf
+
+# data set urls
+IRIS_TRAINING = 'iris_trining.csv'
+IRIS_TRAINING_URL = 'http://download.tensorflow.org/data/iris_training.csv'
+IRIS_TEST = 'iris_test.csv'
+IRIS_TEST_URL = 'http://download.tensorflow.org/data/iris_test.csv'
+
+def fetch_iris_data():
+    if not os.path.exists('./data/iris'):
+        try:
+            os.makedirs('./data/iris')
+        except OSError as e:
+            if e.errno != errno.EEXIST:
+                raise
+    if not os.path.exists('./data/iris/' + IRIS_TRAINING):
+        raw = urlopen(IRIS_TRAINING_URL).read()
+        try:
+            with open('./data/iris/' + IRIS_TRAINING, 'wb') as f:
+                f.write(raw)
+        except IOError:
+            print('could not write ' + IRIS_TRAINING + ' data to file')
+
+    if not os.path.exists('./data/iris/' + IRIS_TEST):
+        raw = urlopen(IRIS_TEST_URL).read()
+        try:
+            with open('./data/iris/' + IRIS_TEST, 'wb') as f:
+                f.write(raw)
+        except IOError:
+            print('could not write ' + IRIS_TEST + ' data to file')
+
+def main():
+    # write data to disk
+    fetch_iris_data()
+
+    # loada datasets
+    training_set = tf.contrib.learn.datasets.base.load_csv_with_header(
+        filename = './data/iris/' + IRIS_TRAINING,
+        target_dtype=np.int,
+        features_dtype=np.float32)
+
+    # check data
+    feature_columns = [tf.feature_column.numeric_column('x', shape=[4])]
+
+    # build 3 layer dnn, with 10,20,10, units
+    classifier = tf.estimator.DNNClassifier(
+        feature_columns=feature_columns,
+        hidden_units=[10, 20, 10],
+        n_classes=3,
+        model_dir='/tmp/iris_model')
+
+    # define the training inputs
+    train_input_fn = tf.estimator.inputs.numpy_input_fn(
+        x={'x': np.array(training_set.data)},
+        y=np.array(training_set.target),
+        num_epochs=None,
+        shuffle=True)
+
+    # train model
+    with tf.device('/gpu:0'):
+        classifier.train(input_fn=train_input_fn, steps=10)
+
+    test_input_fn = tf.estimator.inputs.numpy_input_fn(
+        x={'x': np.array(training_set.data)},
+        y=np.array(training_set.target),
+        num_epochs=None,
+        shuffle=True)
+
+    # evaluate
+    with tf.device('/gpu:0'):
+        accuracy_score = classifier.evaluate(input_fn=test_input_fn)
+
+    print('\nTestAccuracy: {0:f}\n'.format(accuracy_score))
+
+if __name__ == '__main__':
+    main()
---
+++ learning/practice/mnist_net2.py
@@ -0,0 +1,47 @@
+from tensorflow.examples.tutorials.mnist import input_data
+import tensorflow as tf
+
+# mnist data
+mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
+
+sess = tf.InteractiveSession()
+
+# model
+
+x = tf.placeholder(tf.float32, shape=[None, 784])
+y_ = tf.placeholder(tf.float32, shape=[None, 10])
+
+W = tf.Variable(tf.zeros([784, 10]))
+b = tf.Variable(tf.zeros([10]))
+
+sess.run(tf.global_variables_initializer())
+
+# loss function
+
+y = tf.matmul(x, W) + b
+
+cross_entropy = tf.reduce_mean(
+    tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y))
+
+# run training
+
+train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
+
+for _ in range(1000):
+    batch = mnist.train.next_batch(100)
+    train_step.run(feed_dict={x: batch[0], y_: batch[1]})
+
+# evaluate model
+
+correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
+
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+print(accuracy.eval(feed_dict={x: mnist.test.images, y_: mnist.test.labels}))
+
+def weight_variable(shape):
+    initial = tf.truncated_normal(shape, stddev=0.1)
+    return tf.Variable(initial)
+
+def bias_variable(shape):
+    initial = tf.consta(0.1, shape=shape)
