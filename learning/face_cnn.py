a518d4b12a9bdfb3674114ddf0c6036178b7581d
Sean Pollock <seanpollock3344@gmail.com>
2018-01-14 01:48:28 -0600

    pooling layer

--- learning/face_cnn.py
+++ learning/face_cnn.py
@@ -48,10 +48,55 @@ class FaceNet():
         # first convolutional layer
         with tf.name_scope('conv1'):
             W_1 = self.weight_variable([5, 5, 1, 32])
+            b_1 = self.bias_variable([32])
+            h_conv1 = tf.nn.relu(self.conv2d(x_image, W_1) + b_conv1)
+
+        # first pooling layer
+        with.tf.name_scope('pool1'):
+            h_pool1 = self.max_pool_2x2(h_conv1)
 
 
+    def weight_variable(self, shape):
+        # pick random weights in a truncated normal distribution
+        initial = tf.truncated_normal(shape, stddev=0.1)
+        return tf.Variable(initial)
+
+    def bias_variable(self, shape):
+        initial = tf.constant(0.1, shape=shape)
+        return tf.Variable(initial)
+
+    def conv2d(self, x, W):
+        return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
+
+    def max_pool_2x2(x):
+        return tf.nn.max_pool(x, ksise=[1, 2, 2, 1],
+            strides=[1, 2, 2, 1], padding='SAME')
+            
     def train_network(self):
 
     def eval_network(self):
 
     ########################################
+
+'''
+
+GENERAL ARCHITECTURE:
+
+########################################
+CONVOLUTIONAL LAYER
+########################################
+
+########################################
+POOLING LAYER
+########################################
+
+########################################
+CONVOLUTIONAL LAYER
+########################################
+
+########################################
+POOLING LAYER
+########################################
+
+
+'''
