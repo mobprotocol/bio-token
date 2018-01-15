be6c9f87945c53912dbaa773f26aac3ee6543b07
Sean Pollock <seanpollock3344@gmail.com>
2018-01-15 17:47:30 -0600

    resizing images to 100 by 100"

--- scripts/process_yt_data.py
+++ scripts/process_yt_data.py
@@ -1,6 +1,8 @@
 import os
 import cv2
 import codecs
+import dlib
+from find_faces import find_faces
 
 def main():
     '''
@@ -43,8 +45,19 @@ def main():
                         # generate unique id
                         image_id = codecs.encode(os.urandom(32), 'hex').decode()
 
+                        # isolate face
+                        close_up = find_faces(image)
+
+                        # get height and width
+                        h, w = close_up.shape
+
+                        resized_image = cv2.resize(close_up, (100, 100), interpolation=cv2.INTER_CUBIC)
+                        # f
+                        cv2.imshow('image', resized_image)
+                        cv2.waitKey(1)
+
                         # write image to disk
-                        cv2.imwrite('{0}/{1}/{2}.jpg'.format(write_path, person, image_id), image)
+                        cv2.imwrite('{0}/{1}/{2}.jpg'.format(write_path, person, image_id), resized_image)
 
 if __name__ == '__main__':
 
