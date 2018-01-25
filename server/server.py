576a7c4aeb0aa648dbb206db5656e07862f245c8
Sean Pollock <seanpollock3344@gmail.com>
2018-01-25 15:23:16 -0600

    wrapping coroutine in for loop to prevent ws from closing

--- server/server.py
+++ server/server.py
@@ -2,10 +2,12 @@ import asyncio
 import websockets
 
 async def setup_websocket(websocket, path):
-    data = await websocket.recv()
-    print('{0}'.format(data))
-    processed_data = await process_data(data)
-    await websocket.send(processed_data)
+    while True:
+        data = await websocket.recv()
+        print(data)
+    
+        processed_data = await process_data(data)
+        await websocket.send(processed_data)
 
 async def process_data(data):
     return data + ' hey this is the server responding'
