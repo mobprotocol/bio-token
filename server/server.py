2ae4df448b2a564cb02bc2909a57b903c62d1076
Sean Pollock <seanpollock3344@gmail.com>
2018-01-24 20:29:33 -0600

    sending message from server

--- server/server.py
+++ server/server.py
@@ -2,8 +2,13 @@ import asyncio
 import websockets
 
 async def setup_websocket(websocket, path):
-    name = await websocket.recv()
-    print('{0}'.format(name))
+    data = await websocket.recv()
+    print('{0}'.format(data))
+    processed_data = await process_data(data)
+    await websocket.send(processed_data)
+
+async def process_data(data):
+    return data + ' hey this is the server responding'
 
 start_server = websockets.serve(setup_websocket, 'localhost', 3344)
 
