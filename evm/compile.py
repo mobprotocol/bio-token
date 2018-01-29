ed333bf6ec397362b975784e0cf875c97581b703
Sean Pollock <seanpollock3344@gmail.com>
2018-01-28 19:29:24 -0600

    compile and deploy script

--- evm/compile.py
+++ evm/compile.py
@@ -1,5 +1,6 @@
 from solc import compile_standard
 from solc import compile_source, compile_files, link_code
+from web3 import Web3, TestRPCProvider
 import os
 import json
 import sys
@@ -17,11 +18,31 @@ def main():
     # read text from solidity file
     with open(contract_path, 'r') as contract:
         data = contract.read()
-        compile_contract(data)
+
+    # comile contract and return json interface
+    interface = compile_contract(data)
+
+    # setup web3 provider to testrpc
+    w3 = Web3(TestRPCProvider())
+
+    deploy_contract(interface, w3)
+
 
 def compile_contract(contract):
     compiled_data = compile_source(contract)
-    print(compiled_data, type(compiled_data))
+    return compiled_data['<stdin>:{0}'.format(sys.argv[1])]
+
+def deploy_contract(interface, w3):
+    print(interface['bin'])
+    contract = w3.eth.contract(
+        abi=interface['abi'],
+        bytecode=interface['bin'])
+    print(contract, type(contract))
+
+    tx_hash = contract.deploy(transaction={ 'from': w3.eth.accounts[0], 'gas': 410000 })
+    print(tx_hash)
+
+
 
 if __name__ == '__main__':
     #
